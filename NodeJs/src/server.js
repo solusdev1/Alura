97414bd39c79f521bba432f59291a7eb88537bb0
David.Dev <118360819+solusdev1@users.noreply.github.com>
2025-12-14 18:38:21 -0300

    ''

--- NodeJs/src/server.js
+++ NodeJs/src/server.js
@@ -7,6 +7,6 @@ const server = http.createServer((request, response) => {
     return response.end("Olá Mundo")
 })
 
-server.listen(3333, () => {
+server.listen(3334, () => {
   console.log(`🚀 Servidor rodando na porta 3333`)
 })
