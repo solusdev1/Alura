ee34c16ee384d0bfa1b05c2364173d4bf0de16fe
David.Dev <118360819+solusdev1@users.noreply.github.com>
2025-12-14 18:45:31 -0300

    porta server

--- NodeJs/src/server.js
+++ NodeJs/src/server.js
@@ -4,9 +4,19 @@ import http  from "node:http"
 
 
 const server = http.createServer((request, response) => {
+    const { method, url } = request
+    console.log(method, url)
     return response.end("Olá Mundo")
 })
 
-server.listen(3334, () => {
-  console.log(`🚀 Servidor rodando na porta 3333`)
+server.listen(3335, () => {
+  console.log(`🚀 Servidor rodando na porta "3335")`)
+  // 👇 ISSO EVITA O ERRO AO SALVAR
+  
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Servidor encerrado corretamente")
+    process.exit(0)
+  })
+})
 })
