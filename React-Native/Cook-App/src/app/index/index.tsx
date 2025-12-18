3100d3a57902a7002529adbb99b6d73a07a9c89c
David.silva <gestor.davidoliveira@gmail.com>
2025-12-18 17:05:59 -0300

    React Native

---
+++ React-Native/Cook-App/src/app/index/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "./_styles/styles";
+
+export default function index(){
+  return(
+    <View style={styles.container}>
+      <Text> Ola  </Text>
+    </View>
+  );
+}
