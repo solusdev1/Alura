4e49abf5846e56d099dca448354aa46b9782d76e
David.silva <gestor.davidoliveira@gmail.com>
2025-12-26 16:16:03 -0300

    ''

--- Alura-main/Backend/src/index.js
+++ Alura-main/Backend/src/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const trataErros = require('./erros/funcoesErro');
+const trataErros = require('./erros/funcoesErros');
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
